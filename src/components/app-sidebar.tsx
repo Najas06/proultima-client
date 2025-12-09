5dd6645cdf2f3556208fa9cfe5ecd25c48fbbf91
Najas06 <najasneju@gmail.com>
2025-12-10 00:34:50 +0530

    sidebar completed

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,104 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconCamera,
+  IconChartBar,
+  IconDashboard,
+  IconDatabase,
+  IconFileAi,
+  IconFileDescription,
+  IconFileWord,
+  IconFolder,
+  IconHelp,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconReport,
+  IconSearch,
+  IconSettings,
+  IconUsers,
+} from "@tabler/icons-react"
+
+import { NavDocuments } from "@/components/nav-documents"
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+import Image from "next/image"
+import { Activity, Box, Calendar, CreditCard } from "lucide-react"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: IconDashboard,
+    },
+    {
+      title: "Status",
+      url: "/status",
+      icon: Activity,
+    },
+    {
+      title: "Billing & Invoices",
+      url: "/billing-and-invoices",
+      icon: CreditCard,
+    },
+    {
+      title: "Upcoming Submissions",
+      url: "/upcoming-submissions",
+      icon: Calendar,
+    },
+    {
+      title: "Material Management",
+      url: "/material-management",
+      icon: Box,
+    },
+
+  ],
+ 
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5 hover:bg-transparent"
+            >
+              <Link href="/dashboard">
+                <Image src="/logo.png" alt="Proultima Client" width={32} height={32} className="w-6 " />
+                <span className="text-base font-semibold">Proultima Client</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        {/* <NavDocuments items={data.documents} /> */}
+        {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
